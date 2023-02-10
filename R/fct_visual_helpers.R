d335e783e9bff5da8a67cde37fe4f41eae316234
RealCoChenchao <chenchao.zang@usrealco.com>
2023-02-09 21:56:46 -0600

    add a rank table module

--- R/fct_visual_helpers.R
+++ R/fct_visual_helpers.R
@@ -1,7 +1,16 @@
-#' visual_helpers 
+#' visual_helpers
 #'
 #' @description A fct function
 #'
 #' @return The return value, if any, from executing the function.
 #'
 #' @noRd
+format_axio_mkt_metric_tbl <- function(metric_tbl){
+  metric_tbl %>%
+    dplyr::rename(
+      Market = marketname,
+      `Rent Growth %` = mean_effective_rent_per_sf_period_growth,
+      `Occupancy Change %` = mean_occupancy_period_change,
+      `Revenue Per Unit Growth %` = mean_revenue_per_unit_period_growth
+    )
+}
---
+++ R/mod_rank_table.R
@@ -0,0 +1,34 @@
+#' rank_table UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_rank_table_ui <- function(id){
+  ns <- NS(id)
+  DT::dataTableOutput(NS(id, "rank_table"))
+}
+
+#' rank_table Server Functions
+#'
+#' @noRd
+mod_rank_table_server <- function(id, pefm_table){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+    output$rank_table <-
+      DT::renderDataTable(datatable(pefm_table()) %>%
+                            DT::formatPercentage(2:4,
+                                                 2),
+                          options = list(scrollX = TRUE),
+                          rownames = FALSE)
+  })
+}
+
+## To be copied in the UI
+# mod_rank_table_ui("rank_table_1")
+
+## To be copied in the server
+# mod_rank_table_server("rank_table_1")
