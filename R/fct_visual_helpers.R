5239e10dc2840e307250ff6ab64e86b1f9aac279
RealCoChenchao <chenchao.zang@usrealco.com>
2023-02-15 12:01:54 -0600

    Update the format fuction to allow the grade and unit_dist rename

--- R/fct_visual_helpers.R
+++ R/fct_visual_helpers.R
@@ -6,11 +6,23 @@
 #'
 #' @noRd
 format_axio_mkt_metric_tbl <- function(metric_tbl){
-  metric_tbl %>%
+  rename_tbl <- metric_tbl %>%
     dplyr::rename(
       Market = marketname,
       `Rent Growth %` = mean_effective_rent_per_sf_period_growth,
       `Occupancy Change %` = mean_occupancy_period_change,
       `Revenue Per Unit Growth %` = mean_revenue_per_unit_period_growth
     )
+
+  if("property_unit_dist" %in% names(metric_tbl)){
+    rename_tbl <- rename_tbl %>%
+      dplyr::rename(`Unit Dist` = property_unit_dist)
+  }
+
+  if("property_market_grade_new" %in% names(metric_tbl)){
+    rename_tbl <- rename_tbl %>%
+      dplyr::rename(`Grade` = property_market_grade_new)
+  }
+
+  rename_tbl
 }
